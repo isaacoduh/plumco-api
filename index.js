9bb8c6e2bcef393fe9a1d9a8befbcfbb1224a349
Isaac Oduh <ufuomaoduh@gmail.com>
2024-08-30 14:07:15 +0100

    savepoint

--- index.js
+++ index.js
@@ -23,6 +23,28 @@ mongoose
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://your-production-domain.com",
+  "http://localhost:5173",
+]; // Add your allowed origins here
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin, like mobile apps or curl requests
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 // cors configuration
 app.use(cookieParser());
 app.use(express.json());
