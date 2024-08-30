2229b7b7319648883b76e2bf2b23f01357232df2
Isaac Oduh <ufuomaoduh@gmail.com>
2024-08-30 15:49:55 +0100

    fix: refactor endpoint

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -85,7 +85,7 @@ const logout = catchAsyncErrors((req, res) => {
 
 const checkAuth = catchAsyncErrors((req, res) => {
   const user = req.user;
-  res.status(200).json({ message: true, message: "Authenticated User!", user });
+  res.status(200).json({ success: true, message: "Authenticated User!", user });
 });
 
 module.exports = { register, login, logout, checkAuth };
